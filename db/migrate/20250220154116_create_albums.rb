81eabcdbb481943bfb5f299863c1feceda902bcc
Scott Fleming <sdflem@gmail.com>
2025-02-20 09:41:22 -0600

    Complete album-db app

---
+++ db/migrate/20250220154116_create_albums.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateAlbums < ActiveRecord::Migration[7.1]
+  def change
+    create_table :albums do |t|
+      t.string :title
+      t.string :artist
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,69 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Dark Side of the Moon
+
+Album.create!(
+  title:  'The Dark Side of the Moon',
+  artist: 'Pink Floyd'
+)
+
+Track.create!(
+  title:             'Speak to Me / Breathe',
+  length_in_seconds: (3 * 60) + 58
+)
+
+Track.create!(
+  title:             'On the Run',
+  length_in_seconds: (3 * 60) + 35
+)
+
+Track.create!(
+  title:             'Time',
+  length_in_seconds: (7 * 60) + 5
+)
+
+# Rumours
+
+Album.create!(
+  title:  'Rumours',
+  artist: 'Fleetwood Mac'
+)
+
+Track.create!(
+  title:             'Second Hand News',
+  length_in_seconds: (2 * 60) + 48
+)
+
+Track.create!(
+  title:             'Dreams',
+  length_in_seconds: (4 * 60) + 39
+)
+
+Track.create!(
+  title:             'Never Going Back Again',
+  length_in_seconds: (2 * 60) + 14
+)
+
+# Abbey Road
+
+Album.create!(
+  title:  'Abbey Road',
+  artist: 'The Beatles'
+)
+
+Track.create!(
+  title:             'Come Together',
+  length_in_seconds: (4 * 60) + 19
+)
+
+Track.create!(
+  title:             'Something',
+  length_in_seconds: (3 * 60) + 3
+)
+
+Track.create!(
+  title:             'Maxwells Silver Hammer',
+  length_in_seconds: (3 * 60) + 27
+)
